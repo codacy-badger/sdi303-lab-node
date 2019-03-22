150531bd2e015269fa8e8bfe462bbe99f84ec8a8
yagogr98 <yagogr98@gmail.com>
2019-03-22 10:17:50 +0100

    SDI-NodeJS- 11,12,13,14 Vistas y motores de plantillas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -10,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', 8081);
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
 
 // lanzar el servidor
 app.listen(app.get('port'), function() {
